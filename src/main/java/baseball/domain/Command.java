36256e164ef0f233da9a3419e839d221008d7f8b
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-30 15:26:04 +0900

    feat: 게임 진행 결정 데이터를 입력 받는 기능 구현

--- src/main/java/baseball/domain/Command.java
+++ src/main/java/baseball/domain/Command.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import java.util.Arrays;
+
 public enum Command {
     RESTART(1),
     QUIT(2);
@@ -8,5 +10,12 @@ public enum Command {
 
     Command(int num) {
         commandNum = num;
+    }
+
+    public static Command getEnumByInt(int num) {
+        return Arrays.stream(Command.values())
+                .filter(cm -> cm.commandNum == num)
+                .findAny()
+                .orElseThrow(()-> new IllegalArgumentException("[ERROR] 유효하지 않은 COMMAND 숫자의 입력 입니다."));
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,28 +1,35 @@
 package baseball.view;
 
 import baseball.domain.Ball;
+import baseball.domain.Command;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
-    public Ball readNBallByUser(int ballSize){
+    public Ball readNBallByUser(int ballSize) {
         System.out.println("숫자를 입력해주세요 : ");
         String userInput = Console.readLine();
         return new Ball(makeNumberList(userInput));
     }
 
-    private List<Integer> makeNumberList(String userInput){
+    public Command readGameCommand() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userInput = Console.readLine();
+        return Command.getEnumByInt(Integer.parseInt(userInput));
+    }
+
+    private List<Integer> makeNumberList(String userInput) {
         IsNumber(userInput);
         return userInput
                 .chars()
                 .boxed()
-                .map(ch->ch - '0')
+                .map(ch -> ch - '0')
                 .collect(Collectors.toList());
     }
 
-    private void IsNumber(String userInput){
+    private void IsNumber(String userInput) {
         Integer.parseInt(userInput);
     }
 }

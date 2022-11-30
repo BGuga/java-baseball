cb1c29f249d3df5bc79d4b1f68b612f895e94a32
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-30 15:06:23 +0900

    올 스트라이크일 경우 게임이 끝나는 기능 구현

--- src/main/java/baseball/domain/BaseBallGame.java
+++ src/main/java/baseball/domain/BaseBallGame.java
@@ -8,14 +8,22 @@ public class BaseBallGame {
     private final int DEFAULT_MIN = 1;
     private final int DEFAULT_MAX = 9;
     private final int DEFAULT_BALL_SIZE = 3;
+    private boolean gameEnd;
 
     public BaseBallGame() {
         ballMaker = new BallMaker(new RandomNumberMaker(), DEFAULT_MIN, DEFAULT_MAX);
         computerBall = ballMaker.generateNSizeRandomBall(DEFAULT_BALL_SIZE);
+        this.gameEnd = false;
     }
 
     public EnumMap<BallResult, Integer> throwBall(Ball ball) {
+        checkGameStatus(ball);
         return computerBall.getTotalBallResult(ball);
     }
 
+    private void checkGameStatus(Ball ball) {
+        if(computerBall.isAllStrike(ball)){
+            this.gameEnd = true;
+        }
+    }
 }

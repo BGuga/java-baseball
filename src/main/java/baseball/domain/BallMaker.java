06ef3b5901b1ca67e93fffb4f7d68882d40811f5
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-30 14:44:57 +0900

    feat: 공을 만들어 내는 기능 구현

--- src/main/java/baseball/domain/BallMaker.java
+++ src/main/java/baseball/domain/BallMaker.java
@@ -1,4 +1,24 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class BallMaker {
+
+    private final NumberMaker numberMaker;
+    private final int minBallSize;
+    private final int maxBallSize;
+
+    public BallMaker(RandomNumberMaker numberMaker, int min, int max) {
+        this.numberMaker = numberMaker;
+        this.minBallSize = min;
+        this.maxBallSize = max;
+    }
+
+    public Ball generateNSizeRandomBall(int size) {
+        return new Ball(numberMaker.generateNSizeUniqueNumber(size, minBallSize, maxBallSize));
+    }
+
+    public Ball generateBall(List<Integer> ballData) {
+        return new Ball(ballData);
+    }
 }
